76cef675804aaa680b6f5c9dc853834c56cfc5e0
BrokenSt0ne <105749252+BrokenSt0ne@users.noreply.github.com>
2023-07-09 00:49:54 +0100

    Added Source Code

---
+++ Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using HoneyLib;
+using Photon.Pun;
+using System;
+using UnityEngine;
+
+
+namespace GorillaRoulette
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+            HoneyLib.Events.Events.InfectionTagEvent += HoneyLib.Events.Events.InfectionTagEvent;
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            Utilla.Events.GameInitialized -= OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+        }
+        void InfectionTagEvent(object sender, HoneyLib.Events.InfectionTagEventArgs e)
+        {
+            if (e.taggedPlayer.IsLocal)
+            {
+                System.Random random = new System.Random();
+                int randomNumber = random.Next(1, 11);
+                if (randomNumber == 1)
+                {
+                    // Event that should happen with 1 in 10 chance
+                    Application.Quit();
+                }
+                else
+                {
+                    // Event that should happen in the other 9 cases
+                    Console.WriteLine("Survived.");
+                }
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaRoulette
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.brokenstone.gorillatag.gorillaroulette";
+        public const string Name = "GorillaRoulette";
+        public const string Version = "1.0.0";
+    }
+}
